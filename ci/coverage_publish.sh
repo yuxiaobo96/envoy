d87b7cf64ef3a79d4c2bd49002b55bb14d145eea
Lizan Zhou <lizan@tetrate.io>
2019-11-05 05:08:58 +0000

    ci: coverage in azp
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- ci/coverage_publish.sh
+++ ci/coverage_publish.sh
@@ -4,10 +4,6 @@
 # CircleCI logs.
 set -e
 
-if [ "${CIRCLECI}" != "true" ]; then
-  exit 0
-fi
-
 [[ -z "${ENVOY_BUILD_DIR}" ]] && ENVOY_BUILD_DIR=/build
 COVERAGE_FILE="${ENVOY_BUILD_DIR}/envoy/generated/coverage/index.html"
 
@@ -16,18 +12,22 @@ if [ ! -f "${COVERAGE_FILE}" ]; then
   exit 1
 fi
 
-# available for master builds
-if [ -z "$CIRCLE_PR_NUMBER" ]
-then
-  echo "Uploading coverage report..."
-
-  BRANCH_NAME="${CIRCLE_BRANCH}"
-  COVERAGE_DIR="$(dirname "${COVERAGE_FILE}")"
-  GCS_LOCATION="envoy-coverage/report-${BRANCH_NAME}"
-
-  echo ${GCP_SERVICE_ACCOUNT_KEY} | base64 --decode | gcloud auth activate-service-account --key-file=-
-  gsutil -m rsync -dr ${COVERAGE_DIR} gs://${GCS_LOCATION}
-  echo "Coverage report for branch '${BRANCH_NAME}': https://storage.googleapis.com/${GCS_LOCATION}/index.html"
-else
+if [[ -z "${GCP_SERVICE_ACCOUNT_KEY}" ]]; then
   echo "Coverage report will not be uploaded for this build."
+  exit 0
+else
+  echo ${GCP_SERVICE_ACCOUNT_KEY} | base64 --decode | gcloud auth activate-service-account --key-file=-
 fi
+
+echo "Uploading coverage report..."
+
+if [[ "${BUILD_REASON}" == "PullRequest" ]]; then
+  GCS_LOCATION="envoy-pr/${SYSTEM_PULLREQUEST_PULLREQUESTNUMBER}/coverage"
+else
+  GCS_LOCATION="envoy-coverage/report-${BRANCH_NAME}"
+fi
+
+COVERAGE_DIR="$(dirname "${COVERAGE_FILE}")"
+
+gsutil -m rsync -dr ${COVERAGE_DIR} gs://${GCS_LOCATION}
+echo "Coverage report for branch '${BRANCH_NAME}': https://storage.googleapis.com/${GCS_LOCATION}/index.html"
